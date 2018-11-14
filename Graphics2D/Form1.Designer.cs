8f759320622eaf2ca632c848cb51aa8724faf7f5
Quinn <44106439+QuinnLed@users.noreply.github.com>
2018-11-13 19:21:35 -0600

    First Commit

---
+++ Graphics2D/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Graphics2D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblRedScore = new System.Windows.Forms.Label();
+            this.lblBlueScore = new System.Windows.Forms.Label();
+            this.lblPlayerTurn = new System.Windows.Forms.Label();
+            this.lblCoords = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 33;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblRedScore
+            // 
+            this.lblRedScore.AutoSize = true;
+            this.lblRedScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRedScore.ForeColor = System.Drawing.Color.White;
+            this.lblRedScore.Location = new System.Drawing.Point(151, 499);
+            this.lblRedScore.Name = "lblRedScore";
+            this.lblRedScore.Size = new System.Drawing.Size(34, 16);
+            this.lblRedScore.TabIndex = 1;
+            this.lblRedScore.Text = "Red";
+            // 
+            // lblBlueScore
+            // 
+            this.lblBlueScore.AutoSize = true;
+            this.lblBlueScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlueScore.ForeColor = System.Drawing.Color.White;
+            this.lblBlueScore.Location = new System.Drawing.Point(232, 499);
+            this.lblBlueScore.Name = "lblBlueScore";
+            this.lblBlueScore.Size = new System.Drawing.Size(35, 16);
+            this.lblBlueScore.TabIndex = 1;
+            this.lblBlueScore.Text = "Blue";
+            // 
+            // lblPlayerTurn
+            // 
+            this.lblPlayerTurn.AutoSize = true;
+            this.lblPlayerTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerTurn.ForeColor = System.Drawing.Color.White;
+            this.lblPlayerTurn.Location = new System.Drawing.Point(119, 47);
+            this.lblPlayerTurn.Name = "lblPlayerTurn";
+            this.lblPlayerTurn.Size = new System.Drawing.Size(35, 16);
+            this.lblPlayerTurn.TabIndex = 1;
+            this.lblPlayerTurn.Text = "Turn";
+            // 
+            // lblCoords
+            // 
+            this.lblCoords.AutoSize = true;
+            this.lblCoords.BackColor = System.Drawing.Color.White;
+            this.lblCoords.Location = new System.Drawing.Point(765, 478);
+            this.lblCoords.Name = "lblCoords";
+            this.lblCoords.Size = new System.Drawing.Size(40, 13);
+            this.lblCoords.TabIndex = 2;
+            this.lblCoords.Text = "Coords";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(890, 546);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.lblCoords);
+            this.Controls.Add(this.lblPlayerTurn);
+            this.Controls.Add(this.lblBlueScore);
+            this.Controls.Add(this.lblRedScore);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "8-Ball Pool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblRedScore;
+        private System.Windows.Forms.Label lblBlueScore;
+        private System.Windows.Forms.Label lblPlayerTurn;
+        private System.Windows.Forms.Label lblCoords;
+    }
+}
+
---
+++ Graphics2D/sound.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using IrrKlang;
+
+namespace Graphics2D
+{
+    class Sound
+    {
+        #region Class Parameters
+        // Creates the sound engine
+        ISoundEngine engine = new ISoundEngine();
+        #endregion
+
+        #region Class Constructors
+        /// <summary>
+        /// Default sound constructor
+        /// </summary>
+        public Sound() {}
+        #endregion
+
+        #region Class Methods
+        /// <summary>
+        /// Play the sound clip once
+        /// </summary>
+        public void Play()
+        {
+            engine.Play2D(@"C:\Users\Admin\Desktop\Graphics2D\Graphics2D\sound.wav", false);
+        }
+        #endregion
+    }
+}
